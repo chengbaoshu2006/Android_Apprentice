82c1c1aaf778291032564820654fc3dc3e191a74
baoshu.cheng <541013255@qq.com>
2021-12-30 15:38:58 +0800

    Test how to disable title, then test the menu function in android application project. 2021-12-30--->Baoshu.Cheng

--- app/src/main/java/com/example/android_apprentice/MainActivity.java
+++ app/src/main/java/com/example/android_apprentice/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.android_apprentice;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Disable display title on activity, but this method couldn't work if activity extends from AppCompatActivity.
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        // Disable display title on activity, if you want to enable menu feature in the project, please don't hide title.
+        //getSupportActionBar().hide();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Log.d("Menu","Check menu item");
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this, "You click Add item！",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "You click Remove item！",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.check_item:
+                Toast.makeText(this, "You click Check item！",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
